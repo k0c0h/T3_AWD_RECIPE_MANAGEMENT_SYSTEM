b0b51d28a1b1a514bcca6e25b96f55448b37dfc8
Kerlly Alexandra Chiriboga Cabrera <chiribogakerly@gmail.com>
2025-10-26 20:45:42 -0500

    functionalPrototype

--- functionalPrototypes/backend/server.js
+++ functionalPrototypes/backend/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const recipesRouter = require('./routes/recipes');
 const quotesRouter = require('./routes/quotes');
@@ -15,10 +16,15 @@ app.use('/api/recipes', recipesRouter);
 app.use('/api/quotes', quotesRouter);
 app.use('/api/ingredients', ingredientsRouter);
 
+app.use(express.static(path.join(__dirname, '../')));
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log(' Connected to MongoDB Atlas'))
-  .catch(err => console.error('Connection Error:', err));
+  .catch(err => console.error(' Connection Error:', err));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server in ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
