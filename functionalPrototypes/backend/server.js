24ca63aed57647011fbd5eb6e1271eb4832a8e59
Kerlly Alexandra Chiriboga Cabrera <chiribogakerly@gmail.com>
2025-10-26 20:48:17 -0500

    server

--- functionalPrototypes/backend/server.js
+++ functionalPrototypes/backend/server.js
@@ -22,9 +22,10 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log(' Connected to MongoDB Atlas'))
   .catch(err => console.error(' Connection Error:', err));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'));
 });
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
